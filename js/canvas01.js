00a99c2620ff41df907fe17d61ef4e8494357968
Bananek96 <Banasiak.Jakub@wp.pl>
2018-04-18 08:08:33 +0200

    xd

--- js/canvas01.js
+++ js/canvas01.js
@@ -3,3 +3,7 @@ const canvasElem = document.getElementById('canvas');
 const ctx = canvasElem.getContext('2d');
 
 ctx.fillRect(25, 25, 100, 100);
+
+ctx.clearRect(45, 45, 60, 60);
+
+ctx.strokeRect(50, 50, 50, 50);
